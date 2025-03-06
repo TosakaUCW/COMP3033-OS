a5f36a593f98f1c560eb23dafc687178289d5074
TosakaUCW <tosakaucw0@gmail.com>
2025-03-06 14:40:23 +0000

    Task1 Finished

--- Lab2/e2_t1_2330016056.c
+++ Lab2/e2_t1_2330016056.c
@@ -5,17 +5,24 @@
  * @FilePath: /tosaka/COMP3033-OS/Lab2/e2_t1_2330016056.c
  */
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 #define SIZE 1024
 int main(void)
 {
     char prompt[] = "Type a command: ";
     char buf[SIZE];
+    
     // Ask the user to type a command:
-    printf(prompt);
+    // printf(prompt);
+    write(1, prompt, strlen(prompt));
+    
     // Read from the standard input the command typed by the user (note
     // that fgets also puts into the array buf the '\n' character typed
     // by the user when the user presses the Enter key on the keyboard):
-    fgets(buf, SIZE, stdin);
+    // fgets(buf, SIZE, stdin);
+    read(0, buf, SIZE);
+
     // Replace the Enter key typed by the user with '\0':
     for (int i = 0; i < SIZE; i++)
     {
@@ -25,7 +32,12 @@ int main(void)
             break;
         }
     }
+    
     // Execute the command typed by the user (only prints it for now):
-    printf("Executing command: %s\n", buf);
+    // printf("Executing command: %s\n", buf);
+    write(1, "Executing command: ", 20);
+    write(1, buf, strlen(buf));
+    write(1, "\n", 1);
+    
     return 0;
 }
