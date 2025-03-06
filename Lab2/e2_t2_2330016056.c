0f05f8733d9458c9868bebc61bcf5576113bff82
TosakaUCW <tosakaucw0@gmail.com>
2025-03-06 15:23:02 +0000

    Task2 Finished

--- Lab2/e2_t2_2330016056.c
+++ Lab2/e2_t2_2330016056.c
@@ -7,16 +7,17 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/wait.h>
 #define SIZE 1024
 int main(void)
 {
     char prompt[] = "Type a command: ";
     char buf[SIZE];
-    
+
     // Ask the user to type a command:
     // printf(prompt);
     write(1, prompt, strlen(prompt));
-    
+
     // Read from the standard input the command typed by the user (note
     // that fgets also puts into the array buf the '\n' character typed
     // by the user when the user presses the Enter key on the keyboard):
@@ -32,12 +33,23 @@ int main(void)
             break;
         }
     }
-    
+
     // Execute the command typed by the user (only prints it for now):
-    // printf("Executing command: %s\n", buf);
-    write(1, "Executing command: ", 20);
-    write(1, buf, strlen(buf));
-    write(1, "\n", 1);
+
+    // Create child process
+    pid_t pid = fork();
     
+    if (pid == -1) {
+        write(1, "Error: Failed to create child process\n", 38);
+        return 1;
+    } else if (pid == 0) {
+        write(1, "Child: now running the same program as parent, doing exec\n", 58);
+        execlp(buf, buf, (char*)NULL);
+        write(1, "Child: exec failed, die\n", 24);
+        return 1;
+    } else { 
+        wait(NULL);
+    }
+
     return 0;
 }
