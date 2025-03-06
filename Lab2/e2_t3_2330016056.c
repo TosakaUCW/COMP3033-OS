bb8c5dc2148921a20d50bf80087bbe44da22be67
TosakaUCW <tosakaucw0@gmail.com>
2025-03-06 15:35:56 +0000

    Task3 Finished

--- Lab2/e2_t3_2330016056.c
+++ Lab2/e2_t3_2330016056.c
@@ -13,40 +13,41 @@ int main(void) {
     char prompt[] = "Type a command: ";
     char buf[SIZE];
 
-    // Ask the user to type a command:
-    // printf(prompt);
-    write(1, prompt, strlen(prompt));
+    while (1) {
+        // Ask the user to type a command:
+        write(1, prompt, strlen(prompt));
+        read(0, buf, SIZE);
 
-    // Read from the standard input the command typed by the user (note
-    // that fgets also puts into the array buf the '\n' character typed
-    // by the user when the user presses the Enter key on the keyboard):
-    // fgets(buf, SIZE, stdin);
-    read(0, buf, SIZE);
+        // Replace the Enter key typed by the user with '\0':
+        for (int i = 0; i < SIZE; i++) {
+            if (buf[i] == '\n' || buf[i] == '\r') {
+                buf[i] = '\0';
+                break;
+            }
+        }
 
-    // Replace the Enter key typed by the user with '\0':
-    for (int i = 0; i < SIZE; i++) {
-        if (buf[i] == '\n' || buf[i] == '\r') {
-            buf[i] = '\0';
+        if (strcmp(buf, "exit") == 0) {
             break;
         }
-    }
 
-    // Execute the command typed by the user (only prints it for now):
+        // Execute the command typed by the user (only prints it for now):
 
-    // Create child process
-    pid_t pid = fork();
+        // Create child process
+        pid_t pid = fork();
 
-    if (pid == -1) {
-        write(1, "Error: Failed to create child process\n", 38);
-        return 1;
-    } else if (pid == 0) {
-        write(1, "Child: now running the same program as parent, doing exec\n",
-              58);
-        execlp(buf, buf, (char *)NULL);
-        write(1, "Child: exec failed, die\n", 24);
-        return 1;
-    } else {
-        wait(NULL);
+        if (pid == -1) {
+            write(1, "Error: Failed to create child process\n", 38);
+            return 1;
+        } else if (pid == 0) {
+            write(1,
+                  "Child: now running the same program as parent, doing exec\n",
+                  58);
+            execlp(buf, buf, (char *)NULL);
+            write(1, "Child: exec failed, die\n", 24);
+            return 1;
+        } else {
+            wait(NULL);
+        }
     }
 
     return 0;
