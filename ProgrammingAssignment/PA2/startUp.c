50fbbfe9323a58bb2e4a076a262119ed555bf4f8
TosakaUCW <tosakaucw0@gmail.com>
2025-04-15 16:27:18 +0800

    chore: PA2 Initial

---
+++ ProgrammingAssignment/PA2/startUp.c
@@ -0,0 +1,33 @@
+﻿/****************************************************************************
+ *                                                                          *
+ * Author		:  	                                                        *
+ *                                                                          *
+ * Studdent ID	: Console mode (command line) program.                      *
+ *                                                                          *
+ * Date			:                                             			    *
+ *                                                         					*
+ * Purpose		:                                                           *
+ *                                                         					*
+ ****************************************************************************/
+ 
+#include <stdio.h>
+
+void runTask1();
+void runTask2();
+
+int main(){
+	while(1){
+		printf("\nEnter a task number (1 or 2) or 0 to exit: ");
+		int taskNo = 0;
+		scanf("%d", &taskNo);
+		switch(taskNo){
+				case 0: exit(0);
+				case 1: runTask1(); break;
+				case 2: runTask2(); break;
+				default: printf("Please enter 0, 1 or 2.\n");
+		}
+	}
+}
+
+void runTask1(){}
+void runTask2(){}
